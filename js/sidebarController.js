068b66d6505f6910387b78fe7f7888162ea2ae36
Rochev Alexandr <velloso3@yandex.ru>
2021-02-10 14:16:42 +0300

    * измене принцип открытия меню

---
+++ js/sidebarController.js
@@ -0,0 +1,35 @@
+const OPEN_CLASSNAME = 'menu-plate_opened'
+
+class sidebarController {
+  constructor() {
+    this.isOpened = false
+    this.sidebar = document.querySelector('.menu-plate-js')
+  }
+
+  clickOutsideHandler = (event) => {
+    if (!this.sidebar.contains(event.target)) {
+      event.preventDefault()
+      this.close()
+    }
+  }
+
+  open = () => {
+    if (this.isOpened) return
+    this.sidebar.classList.add(OPEN_CLASSNAME)
+    document.addEventListener('click', this.clickOutsideHandler)
+    this.isOpened = true
+  }
+
+  close = () => {
+    if (!this.isOpened) return
+    this.sidebar.classList.remove(OPEN_CLASSNAME)
+    document.removeEventListener('click', this.clickOutsideHandler)
+    this.isOpened = false
+  }
+
+  toggle = () => {
+    this.isOpened ? this.close() : this.open()
+  }
+}
+
+const sideBar = new sidebarController()
